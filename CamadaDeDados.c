ff1b891d1a17aaf9f6fe9abffe63f1dab2e1add0
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-03-10 18:54:14 +0000

    Camada de dados.1

--- CamadaDeDados.c
+++ CamadaDeDados.c
@@ -21,7 +21,6 @@ typedef struct {
 		int jogador_atual;
 	} ESTADO;
 
-
 /*
 tab armazena informação sobre o tabuleiro;
 
@@ -32,3 +31,29 @@ jogadas armazena informação sobre as jogadas
 num_jogadas indica quantas jogadas foram efetuadas
 
 jogador_atual indica qual é o jogador a jogar*/
+
+
+/*Pretende-se também que crie funções que alterem a estrutura de dados. Todas
+estas funções devem receber um apontador para ESTADO e modificá-lo. Só
+estas funções deverão alterar o estado! Todo o resto do programa só deverá
+modificar o estado através destas funções. Segue-se um exemplo dos protótipos
+das funções que deverão ser criadas (podendo existir outras):*/
+
+
+ESTADO *inicializar_estado();
+int obter_jogador_atual(ESTADO *estado);
+int obter_numero_de_jogadas(ESTADO *estado);
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
+
+
+/*
+inicializar_estado Esta função deverá criar um estado vazio (com o tabuleiro
+inicializado)
+
+obter_jogador_atual Esta função permite obter o número do jogador atual
+
+obter_estado_casa Esta função permite obter o estado atual da casa
+
+obter_numero_de_jogadas Esta função permite obter quantas jogadas
+
+foram efetuadas (cada jogada tem o movimento de dois jogadores)*/
