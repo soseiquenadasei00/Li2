facbaa2b51a2a4ab2365854b3a6698a53c889374
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-03-10 18:03:49 +0000

    Camada de dados

---
+++ AuxCDD.h
@@ -0,0 +1,12 @@
+
+
+#ifndef CAMADADEDADOS_AUXCDD_H
+#define CAMADADEDADOS_AUXCDD_H
+
+void tabArmazena ();
+void ultima_jogada ();
+void jogadasArmazena ();
+void num_jogadas ();
+void jogador_atual ();
+
+#endif // CAMADADEDADOS_AUXCDD_H
