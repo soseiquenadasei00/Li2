345fdf4c9c85b75f473b274414a35c5535f32009
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-03-11 16:41:59 +0000

    Pastas e codigos

---
+++ Interface/interpretador.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUF_SIZE 1024
+
+
+
+
+// Função que deve ser completada e colocada na camada de interface
+void mostrar_tabuleiro(ESTADO *e) {
+}
+
+// Função que deve ser completada e colocada na camada de interface
+int interpretador(ESTADO *e) {
+char linha[BUF_SIZE];
+char col[2], lin[2];
+if(fgets(linha, BUF_SIZE, stdin) == NULL)
+return 0;
+}
+if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+COORDENADA coord = {*col - 'a', *lin - '1'};
+jogar(e, coord);
+mostrar_tabuleiro(e);
+}
+return 1;
--- Lógica do programa/LogicaDoPrograma.c
+++ Lógica do programa/LogicaDoPrograma.c
@@ -7,3 +7,9 @@ int jogar(ESTADO *estado, COORDENADA c);
 o estado ao jogar na casa correta se a jogada for válida. A função devolve
 verdadeiro (valor diferente de zero) se for possível jogar e falso (zero) caso
 não seja possível.*/
+
+// Função que deve ser completada e colocada na camada da lógica do programa
+int jogar(ESTADO *e, COORDENADA c) {
+printf("jogar %d %d\n", c.coluna, c.linha);
+return 1;
+}
