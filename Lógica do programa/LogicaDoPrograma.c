982e232f1399bec0ec952479ce5810517876a336
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-03-11 16:04:18 +0000

    Pasta de Logica

---
+++ Lógica do programa/LogicaDoPrograma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int jogar(ESTADO *estado, COORDENADA c);
+
+/*jogar esta função deverá receber o estado atual e uma coordenada e modificar
+o estado ao jogar na casa correta se a jogada for válida. A função devolve
+verdadeiro (valor diferente de zero) se for possível jogar e falso (zero) caso
+não seja possível.*/
