88ca44384308bc21433aef5a7d674b0451d41fa1
VitorLelis <60986377+VitorLelis@users.noreply.github.com>
2020-03-12 23:40:40 +0000

    Update jogada_sem_checar.c

--- jogada_sem_checar.c
+++ jogada_sem_checar.c
@@ -1,5 +1,14 @@
-printf ("\nLinhas(0 -> 7 esq p/ dir)\n""Colunas(0 -> 7 cima p/ baixo)\n"
-                                           "\nJogada(1ºlinha 2ºcoluna):");
+typedef struct {
+    int linha;
+    int coluna;}COORDENADA;
+
+void jogada(char tabuleiro[8][8])
+{
+    COORDENADA c1;
+
+    printf ("\nLinhas(0 -> 7 esq p/ dir)\n""Colunas(0 -> 7 cima p/ baixo)\n"
+            "\nJogada(1ºlinha 2ºcoluna):");
     scanf("%d%d", &c1.linha, &c1.coluna);
 
     tabuleiro [c1.linha][c1.coluna] = '*';
+}
