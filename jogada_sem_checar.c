0cf369cd69be9f62a40088c08261a1d5645e14c8
VitorLelis <60986377+VitorLelis@users.noreply.github.com>
2020-03-12 23:34:36 +0000

    Create jogada_sem_checar.c

---
+++ jogada_sem_checar.c
@@ -0,0 +1,5 @@
+printf ("\nLinhas(0 -> 7 esq p/ dir)\n""Colunas(0 -> 7 cima p/ baixo)\n"
+                                           "\nJogada(1ºlinha 2ºcoluna):");
+    scanf("%d%d", &c1.linha, &c1.coluna);
+
+    tabuleiro [c1.linha][c1.coluna] = '*';
