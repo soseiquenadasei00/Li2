fc56ebf2353ed2ace119d5530c920118317fba84
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-03-13 17:23:27 +0000

    interface.h

--- interface1.h
+++ interface1.h
@@ -1,6 +1,6 @@
+
 #ifndef TRABALHOLI2_INTERFACE_H
 #define TRABALHOLI2_INTERFACE_H
-void tab_inicial(char tabuleiro[8][8]);
-void imprimir_tabuleiro (char tab[8][8]);
+void mostrar_tabuleiro(ESTADO *e);
 int interpretador(ESTADO *e);
 #endif //TRABALHOLI2_INTERFACE_H
--- logica1.c
+++ logica1.c
@@ -4,6 +4,6 @@
 int jogar(ESTADO *e, COORDENADA x) {
     printf("jogar %d %d\n", x.letra, x.linha);
     //Busca colocar a peça branca na coordenada especifica buscando está informação no estado
-    e->tab[x.letra][x.linha]=BRACA;
+    e->tab[x.letra][x.linha]=BRANCA;
     return 1;
 }
