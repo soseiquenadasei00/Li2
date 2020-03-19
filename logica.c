8e0f40e36f3ea74bcc5863fe75fcec9c0d2698d9
lelis_vitor <vitorll909@gmail.com>
2020-03-19 16:59:04 +0000

    jogada com peca preta

--- logica.c
+++ logica.c
@@ -7,7 +7,17 @@ int jogar(ESTADO *e, COORDENADA x) {
     return 1;
 }
 
-/*int pecaPreta (ESTADO *e COORDENADA c){
-    e->C='#';
+int pecaPreta(ESTADO *e, COORDENADA c){
+
+    int i = e->ultima_jogada.linha;
+    int j = e->ultima_jogada.letra;
+    e->tab[i][j] = PRETA;
+
+    e->tab[c.linha][c.letra] = BRANCA;
+
+    e->ultima_jogada.linha = c.linha;
+    e->ultima_jogada.letra = c.letra;
+
+
     return 1;
-}*/
+}
--- logica.h
+++ logica.h
@@ -2,4 +2,5 @@
 #ifndef LI2_LOGICA_H
 #define LI2_LOGICA_H
 int jogar(ESTADO *e, COORDENADA c);
+int pecaPreta(ESTADO *e, COORDENADA c);
 #endif //LI2_LOGICA_H
