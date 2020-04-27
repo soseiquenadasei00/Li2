6059b26b2bb9e3d916ae23765e63b631faf5f2bb
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-04-27 12:33:19 +0100

    Documentação

---
+++ cmake-build-debug/CMakeFiles/Li2.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pedro/Documents/Uminho/Li2/Li2/camadaDeDados.c" "/home/pedro/Documents/Uminho/Li2/Li2/cmake-build-debug/CMakeFiles/Li2.dir/camadaDeDados.c.o"
+  "/home/pedro/Documents/Uminho/Li2/Li2/interface.c" "/home/pedro/Documents/Uminho/Li2/Li2/cmake-build-debug/CMakeFiles/Li2.dir/interface.c.o"
+  "/home/pedro/Documents/Uminho/Li2/Li2/lista.c" "/home/pedro/Documents/Uminho/Li2/Li2/cmake-build-debug/CMakeFiles/Li2.dir/lista.c.o"
+  "/home/pedro/Documents/Uminho/Li2/Li2/logica.c" "/home/pedro/Documents/Uminho/Li2/Li2/cmake-build-debug/CMakeFiles/Li2.dir/logica.c.o"
+  "/home/pedro/Documents/Uminho/Li2/Li2/main.c" "/home/pedro/Documents/Uminho/Li2/Li2/cmake-build-debug/CMakeFiles/Li2.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Li2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Li2.dir/camadaDeDados.c.o"
+  "CMakeFiles/Li2.dir/interface.c.o"
+  "CMakeFiles/Li2.dir/lista.c.o"
+  "CMakeFiles/Li2.dir/logica.c.o"
+  "CMakeFiles/Li2.dir/main.c.o"
+  "Li2"
+  "Li2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Li2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
