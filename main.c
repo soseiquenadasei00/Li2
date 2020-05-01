2fe7ec001b45848e01cdd9cf9b81502129583289
Pedro Saldanha <pedrovsaldanha00@gmail.com>
2020-05-01 22:06:20 +0100

    Documentação XML

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 int main()
 {
-    //LISTA d = criar_lista();
     ESTADO *e = inicializar_estado();
     printf("Se quiser sair do jogo digite (Quit)\n\nEste é o tabuleiro inicial:\n\n");
     mostrar_tabuleiro(e);
